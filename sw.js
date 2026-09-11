a80a8c5992de1f1d643dba2ebcc1c8d95ad4da7b
Adirossss <adir23904@gmail.com>
2026-09-11 18:57:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'apay-tracker-v24';
+const CACHE = 'apay-tracker-v25';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
