9f8b1dd8e315c9594039580261bb6a32a013760b
Adirossss <adir23904@gmail.com>
2026-09-12 00:11:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'apay-tracker-v25';
+const CACHE = 'apay-tracker-v26';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
@@ -14,10 +14,6 @@ self.addEventListener('activate', e => {
     caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
   self.clients.claim();
-});
-
-self.addEventListener('message', e => {
-  if(e.data === 'SKIP_WAITING') self.skipWaiting();
 });
 
 self.addEventListener('fetch', e => {
